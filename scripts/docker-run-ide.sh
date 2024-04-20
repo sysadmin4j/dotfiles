b3ef635fff1e14d5e70da40a02cf57c38e9e11e9
Felix Langelier <sysadmin4j@users.noreply.github.com>
2024-04-20 12:40:00 -0400

    fix indent

--- scripts/docker-run-ide.sh
+++ scripts/docker-run-ide.sh
@@ -19,8 +19,8 @@ function log_err () {
 }
 
 function load_x11() {
-    log_debug "loading X11"
-    ${X11_COMMAND}
+  log_debug "loading X11"
+  ${X11_COMMAND}
 }
 
 # load X11 if enabled
@@ -28,24 +28,23 @@ function load_x11() {
 
 # if nvim is the first script arg and there are more than one arg
 if [ "$1" == "nvim" ] && [ "$#" -gt 1 ]; then
-	IMAGE_CMD="nvim"
-	shift
-	for FILE_TO_OPEN in "$@"; do
+  IMAGE_CMD="nvim"
+  shift
+  for FILE_TO_OPEN in "$@"; do
     FILE_TO_OPEN_ABSOLUTE_PATH=$(readlink -f ${FILE_TO_OPEN})
-		# check if the file exist
-		if [ -f "${FILE_TO_OPEN_ABSOLUTE_PATH}" ]; then
-			# adding the file to the volume options
-			FILE_TO_OPEN_VOLUME_OPTS="-v ${FILE_TO_OPEN_ABSOLUTE_PATH}:${FILE_TO_OPEN_ABSOLUTE_PATH} ${FILE_TO_OPEN_VOLUME_OPTS}"
-			IMAGE_CMD="$IMAGE_CMD ${FILE_TO_OPEN_ABSOLUTE_PATH}"
-		else
+    # check if the file exist
+    if [ -f "${FILE_TO_OPEN_ABSOLUTE_PATH}" ]; then
+      # adding the file to the volume options
+      FILE_TO_OPEN_VOLUME_OPTS="-v ${FILE_TO_OPEN_ABSOLUTE_PATH}:${FILE_TO_OPEN_ABSOLUTE_PATH} ${FILE_TO_OPEN_VOLUME_OPTS}"
+      IMAGE_CMD="$IMAGE_CMD ${FILE_TO_OPEN_ABSOLUTE_PATH}"
+    else
       # log an error and quit if one of the files specified in argument(s) don't exist(s)
-			log_err "File ${FILE_TO_OPEN} don't exist"
-			exit 1
-		fi
-	done
+      log_err "File ${FILE_TO_OPEN} don't exist"
+      exit 1
+    fi
+  done
 fi
 
-# log debugging informations
 log_debug "FILE_TO_OPEN_VOLUME_OPTS=${FILE_TO_OPEN_VOLUME_OPTS}"
 log_debug "IMAGE_CMD=${IMAGE_CMD}"
 
