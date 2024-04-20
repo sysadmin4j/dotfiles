366cdc68a2fbf852dc8346c3664bfb5086cecf7c
Felix Langelier <sysadmin4j@users.noreply.github.com>
2024-04-19 22:52:56 -0400

    variable rename and X11 support

--- scripts/docker-run-ide.sh
+++ scripts/docker-run-ide.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-IDE_IMAGE_NAME="${IDE_IMAGE_NAME:-ide}"
-IDE_IMAGE_VERSION="${IDE_IMAGE_VERSION:-latest}"
-IDE_IMAGE_CMD="$@"
+IMAGE_NAME="${IDE_IMAGE_NAME:-ide}"
+IMAGE_VERSION="${IDE_IMAGE_VERSION:-latest}"
+IMAGE_CMD="$@"
 DEBUG="${IDE_DEBUG:-false}"
 LOG_CMD="logger -t $1 -s"
+X11="${IDE_X11:-false}"
+X11_COMMAND="xhost +localhost"
 
-function log_debug ()
-{
+function log_debug () {
   if [ "${DEBUG}" == "true" ]; then
     ${LOG_CMD} -p user.debug "$@"
   fi
 }
 
-function log_err ()
-{
+function log_err () {
   ${LOG_CMD} -p user.err "$@"
 }
 
+function load_x11() {
+    log_debug "loading X11"
+    ${X11_COMMAND}
+}
+
+# load X11 if enabled
+[ "${X11}" == "true" ] && load_x11
+
 # if nvim is the first script arg and there are more than one arg
 if [ "$1" == "nvim" ] && [ "$#" -gt 1 ]; then
-	IDE_IMAGE_CMD="nvim"
+	IMAGE_CMD="nvim"
 	shift
 	for FILE_TO_OPEN in "$@"; do
     FILE_TO_OPEN_ABSOLUTE_PATH=$(readlink -f ${FILE_TO_OPEN})
@@ -28,7 +36,7 @@ if [ "$1" == "nvim" ] && [ "$#" -gt 1 ]; then
 		if [ -f "${FILE_TO_OPEN_ABSOLUTE_PATH}" ]; then
 			# adding the file to the volume options
 			FILE_TO_OPEN_VOLUME_OPTS="-v ${FILE_TO_OPEN_ABSOLUTE_PATH}:${FILE_TO_OPEN_ABSOLUTE_PATH} ${FILE_TO_OPEN_VOLUME_OPTS}"
-			IDE_IMAGE_CMD="$IDE_IMAGE_CMD ${FILE_TO_OPEN_ABSOLUTE_PATH}"
+			IMAGE_CMD="$IMAGE_CMD ${FILE_TO_OPEN_ABSOLUTE_PATH}"
 		else
       # log an error and quit if one of the files specified in argument(s) don't exist(s)
 			log_err "File ${FILE_TO_OPEN} don't exist"
@@ -39,7 +47,7 @@ fi
 
 # log debugging informations
 log_debug "FILE_TO_OPEN_VOLUME_OPTS=${FILE_TO_OPEN_VOLUME_OPTS}"
-log_debug "IDE_IMAGE_CMD=${IDE_IMAGE_CMD}"
+log_debug "IMAGE_CMD=${IMAGE_CMD}"
 
 docker run -it --rm \
 	-u $(id -u ${USER}):$(id -g ${USER}) \
@@ -53,5 +61,5 @@ docker run -it --rm \
 	-w /home/${USER} \
 	--cap-add=SYS_PTRACE \
 	--net=host \
-	${IDE_IMAGE_NAME}:${IDE_IMAGE_VERSION} \
-	${IDE_IMAGE_CMD}
+	${IMAGE_NAME}:${IMAGE_VERSION} \
+	${IMAGE_CMD}
