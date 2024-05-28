200a208d0ed1116354d95dba315e84744915972c
Felix Langelier <sysadmin4j@users.noreply.github.com>
2024-05-27 21:51:03 -0400

    Update lazy.lua

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -2,7 +2,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=v10.20.5", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=v10.19.0", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
